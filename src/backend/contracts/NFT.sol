6c12a4e5475f828812d4f7c2eb6308465fe74f5b
Joseph Reynolds <72422816+JosephJamesReynolds@users.noreply.github.com>
2024-04-24 21:43:34 -0700

    Create Contracts/Test

--- src/backend/contracts/NFT.sol
+++ src/backend/contracts/NFT.sol
@@ -1,4 +1,4 @@
-// SPDX-Licence Indentifier: MIT
+// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.4;
 
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
@@ -8,5 +8,10 @@ contract NFT is ERC721URIStorage {
 
     constructor() ERC721("DApp NFT", "DAPP") {}
 
-    function mint(string memory _tokenURI)
+    function mint(string memory _tokenURI) external returns (uint) {
+        tokenCount++;
+        _safeMint(msg.sender, tokenCount);
+        _setTokenURI(tokenCount, _tokenURI);
+        return (tokenCount);
+    }
 }
