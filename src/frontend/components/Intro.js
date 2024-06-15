e5d02275d00cff631bcef38c27b7f878a9fee72c
Joseph Reynolds <72422816+JosephJamesReynolds@users.noreply.github.com>
2024-06-15 02:21:24 -0700

    Project Complete

--- src/frontend/components/Intro.js
+++ src/frontend/components/Intro.js
@@ -2,20 +2,34 @@ import React from "react";
 
 const Intro = () => {
   return (
-    <div
-      className="video-container"
-      style={{
-        width: "100%",
-        height: "100vh",
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <video width="750" height="500" controls autoplay>
-        <source src="YOUR_VIDEO_URL_HERE.mp4" type="video/mp4" />
-        Your browser does not support the video tag.
-      </video>
+    <div className="flex flex-col items-center justify-center w-full h-screen">
+      <div className="mb-5">
+        {" "}
+        {/* Adjust bottom margin as needed */}
+        <h1 className="text-4xl mb-8">
+          {" "}
+          {/* Adjust bottom margin to control space between text and video */}
+          Wanna get started? Watch the video below.
+        </h1>
+        <iframe
+          className="border-2 border-gray-400"
+          width="750"
+          height="500"
+          src="https://www.youtube.com/embed/nIaH8G0XUhU"
+          title="YouTube video player"
+          frameBorder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+          allowFullScreen
+        ></iframe>
+      </div>
+      <a
+        href="https://youtu.be/Ya4aLRYMHAI"
+        target="_blank"
+        rel="noopener noreferrer"
+        className="mt-5 bg-blue-500 text-white font-bold py-2 px-4 rounded hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50 transition ease-in-out duration-150"
+      >
+        Watch my other video
+      </a>
     </div>
   );
 };
